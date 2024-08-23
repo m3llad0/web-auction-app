4b129794dbe51af6e89dfb6be06e4a12b28148ae
m3llad0 <a01655451@tec.mx>
2024-08-23 17:02:11 -0600

    using children, fixed bug

--- frontend/src/components/ui/box.tsx
+++ frontend/src/components/ui/box.tsx
@@ -9,6 +9,7 @@ export default function Box({ children, className}: BoxProps) {
     <div className={`flex items-center bg-white rounded-lg border border-qt_mid py-4 px-6  ${className}`}>
       <div className="flex items-stretch">
         <div className="ml-4 flex flex-col justify-center">
+          {children}
         </div>
       </div>
     </div>
