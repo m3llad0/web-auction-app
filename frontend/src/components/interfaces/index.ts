2c03bc3d4678c9b571c99c91c5e1e0435b718db8
m3llad0 <a01655451@tec.mx>
2024-08-26 00:29:11 -0600

    Created interfaces directory in components

---
+++ frontend/src/components/interfaces/index.ts
@@ -0,0 +1,1 @@
+export type {Product as Product} from "./product";
---
+++ frontend/src/components/interfaces/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: string;
+    product_name: string;
+    current_bid: number;
+    description: string;
+    starting_date: string;
+    finish_date: string;
+    created_by: string;
+    img: string;
+}
